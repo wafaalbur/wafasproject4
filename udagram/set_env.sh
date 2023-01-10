6f741c37327c522eb1f16121d2a58e779a62d0d7
Wafa Albur <wafaalbur21@gmail.com>
2023-01-10 09:36:48 +0300

    Initial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-1.cqleum5ssiv6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+
+export AWS_BUCKET=arn:arn:aws:s3:::wafasbucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=wafasproject
+export URL=http://localhost:8100
